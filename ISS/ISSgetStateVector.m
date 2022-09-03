d0a1db3f4cf7c274e085fcc47b0bed48535f1246
weuniok <weunalami@gmail.com>
2022-09-04 01:17:05 +0200

    adds ISS position generation

--- ISS/ISSgetStateVector.m
+++ ISS/ISSgetStateVector.m
@@ -1,4 +1,4 @@
-function [r,v] = ISSgetStateVector (anomaly)
+function [r,v] = ISSgetStateVector (anomaly, mu)
 % returns state vector of ISS orbit from the following TLE
 
 % 1 25544U 98067A   22246.67365757  .00002926  00000-0  59194-4 0  9993
@@ -17,88 +17,22 @@ function [r,v] = ISSgetStateVector (anomaly)
 deg = pi/180;
 
 secondLine = [2 25544  51.6442 307.7584 0002851 188.8255 322.7546 15.50015618357355];
+
+
 inclination = secondLine(3) * deg;
 RAofAscendingNode = secondLine(4) * deg;
-eccentricity = secondLine(5);
+eccentricity = secondLine(5) * 10E-8;
 perigeeArgument = secondLine(6) * deg;
 
+meanMotion = secondLine(8) * 2*pi/24/60/60; % revolutions/day -> rad/s
 
-h = orbitalElements(1);
-i = orbitalElements(2);
-omega = orbitalElements(3);
-e = orbitalElements(4);
-w = orbitalElements(5);
+semiMajorAxis = (mu / meanMotion^2)^(1/3);
+semiMinorAxis = semiMajorAxis * (1-eccentricity^2)^0.5;
+h = meanMotion * semiMajorAxis * semiMinorAxis;
 
-%radii
-% perigee
-rp = h^2/mu * 1/(1+e*cos(0));
-%apogee
-ra = h^2/mu * 1/(1+e*cos(pi));
-
-ra = -ra;
-rp = -rp;
-
-% semimajor axis
-a = 0.5 * (rp + ra);
-% period
-T = 2 * pi / sqrt(mu) * a^1.5;
-
-% start plot at X, then transform
-t = linspace(0, 2*pi);
-b = a * sqrt(1-e^2);
-
-X = a * cos(t);
-Y = b * sin(t);
-Z = a * 0;
-
-x = (-rp+ra)/2 + X;
-y = Y; 
-z = X*0;
-
-rotation = ...
-    [cos(w) sin(w) 0; -sin(w) cos(w) 0; 0 0 1] * ...
-    [1 0 0; 0 cos(i) sin(i); 0 -sin(i) cos(i)] * ...
-    [cos(omega) sin(omega) 0; -sin(omega) cos(omega) 0; 0 0 1];
-
-R = rotation.' * [x;y;z];
-
-hold on
-% plotting Earth
-earthR = 6371; %km
-[X,Y,Z] = sphere;
-X = X * earthR;
-Y = Y * earthR;
-Z = Z * earthR;
-surf(X,Y,Z, 'DisplayName', 'Earth'); 
-% Equatorial plane
-maxR = max(abs(R), [], 'all');
-[x, y] = meshgrid(-maxR:maxR:maxR);
-z = zeros(size(x,1));
-surf(x,y,z, 'FaceAlpha', 0.2, 'DisplayName', 'Equatorial plane');
-% plotting the orbit
-plot3(R(1,:), R(2,:), R(3,:), 'DisplayName', 'Orbit'); % rotated orbit
-% plot3(R2(1,:), R2(2,:), R2(3,:)); % rotated orbit
-
-% Apse line
-apsePoints = [ [-rp; 0; 0], [0; 0; 0], [ra; 0; 0] ];
-apsePoints = rotation.' * apsePoints;
-plot3(apsePoints(1,:), apsePoints(2,:), apsePoints(3,:), 'o--k', 'MarkerFaceColor', 'black', ...
-    'DisplayName', 'Apse line')
-
-% Node line
-
-
-% Initial state
-plot3(r(1), r(2), r(3), 'o', 'MarkerFaceColor', 'red', 'DisplayName', 'Initial state')
-% Axes
-axis equal
-xlabel('x [km]')
-ylabel('y [km]')
-zlabel('z [km]')
-grid on
-legend()
-% Initial state geo
-
+orbitalElements = [h, inclination, RAofAscendingNode, eccentricity, perigeeArgument, anomaly];
+[r,v] = orbitalElements2state(orbitalElements, mu);
+% plotFromOE(orbitalElements, mu, r);
 
 return
 end
--- orbitalElements2state.m
+++ orbitalElements2state.m
@@ -17,19 +17,19 @@ w = orbitalElements(5);
 theta = orbitalElements(6);
 
 % perifocal coordinates orbital state
-r = h^2/mu * 1 / (1+ e*cos(theta)) * [cos(theta), sin(theta), 0];
-v = mu/h * [-sin(theta), e+cos(theta), 0];
+r = h^2/mu * 1 / (1+ e*cos(theta)) .* [cos(theta); sin(theta); 0];
+v = mu/h * [-sin(theta); e+cos(theta); 0];
 
 % rotation matrix from geocentric to perifocal;
 rotation = ...
     [cos(w) sin(w) 0; -sin(w) cos(w) 0; 0 0 1] * ...
     [1 0 0; 0 cos(i) sin(i); 0 -sin(i) cos(i)] * ...
     [cos(omega) sin(omega) 0; -sin(omega) cos(omega) 0; 0 0 1];
-rotation = rotation.';
 
 % geocentric orbital state
 r = rotation.' * r;
 v = rotation.' * v;
 
+return
 end
 
